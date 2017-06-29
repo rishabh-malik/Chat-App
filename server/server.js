1994e228cb8ed0ff197912bb31508f87211a6742
rishabh-malik <rishabhmalik249@gmail.com>
2017-06-29 21:05:07 +0530

    adding socket.io

--- server/server.js
+++ server/server.js
@@ -1,15 +1,29 @@
 const path=require('path');
+const http=require('http');
 const express=require('express')
 //for setting the app for heroku
 const port=process.env.PORT || 3000;
+const socketIO=require('socket.io');
 
 //for going into the public folder where index.html is
 const publicPath = path.join(__dirname,'../public');
 
 var app=express();
+var server=http.createServer(app);
+var io=socketIO(server);
+
 //creating middleware
 app.use(express.static(publicPath));
 
-app.listen(port,()=>{
+// whenever a new user is connected
+io.on('connection',(socket)=>{
+    console.log('User connected');
+});
+
+io.on('disconnect',()=>{
+    console.log('User was disconnected');
+});
+
+server.listen(port,()=>{
     console.log(`Server is up at port ${port}`);
 });
