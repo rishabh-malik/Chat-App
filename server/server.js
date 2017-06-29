71c25eb366f91a95b8059c965fbaaeac6a2fa890
rishabh-malik <rishabhmalik249@gmail.com>
2017-06-29 20:38:23 +0530

    setting up things for heroku

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const path=require('path');
 const express=require('express')
+//for setting the app for heroku
+const port=process.env.PORT || 3000;
 
 //for going into the public folder where index.html is
 const publicPath = path.join(__dirname,'../public');
@@ -8,6 +10,6 @@ var app=express();
 //creating middleware
 app.use(express.static(publicPath));
 
-app.listen(3000,()=>{
-    console.log('Server is up at port 3000');
+app.listen(port,()=>{
+    console.log(`Server is up at port ${port}`);
 });
