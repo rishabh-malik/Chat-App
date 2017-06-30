0f040e3f4be9ac53829fe070b8c5aad3a27b9ca2
rishabh-malik <rishabhmalik249@gmail.com>
2017-06-30 15:46:52 +0530

    customising user  experience

--- public/js/index.js
+++ public/js/index.js
@@ -24,14 +24,14 @@ socket.on('newLocationMessage',function(message){
         li.append(a);
         jQuery('#messages').append(li);
         });
-
+var messageTextbox=jQuery('[name=message]');
 jQuery('#message-form').on('submit',function(e){
         e.preventDefault();
         socket.emit('createMessage',{
                 from:'User',
-                text:jQuery('[name=message]').val()
+                text:messageTextbox.val()
 },function(){
-    
+    messageTextbox.val('');
 });
         })
 
@@ -41,13 +41,16 @@ locationButton.on('click',function(){
   if(!navigator.geolocation){
     return alert('Geolocation not supported by your browser');
   }
+  locationButton.attr('disabled','disabled').text('Sendling location..');
   //getting the co-ordinates
   navigator.geolocation.getCurrentPosition(function(position){
+   locationButton.removeAttr('disabled').text('Send location');
    socket.emit('createLocationMessage',{
            latitude: position.coords.latitude,
            longitude: position.coords.longitude
    });
   },function(){
+          locationButton.removeAttr('disabled').text('Send location');
           alert('Unable to fetch location');
   })
 })
